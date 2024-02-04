8bcc254b7eac3a3a97f1973fbda54f932ab298a5
bkmz2000 <bkmz2000@gmail.com>
2024-02-04 19:10:49 +0400

    Update task6.go

--- task6/task6.go
+++ task6/task6.go
@@ -1,9 +1,40 @@
 package main
 
-func end() {
-	// Some code
+import (
+	"context"
+	"math/rand"
+)
+
+func just_end(c chan int) {
+	for i := 0; i < 100; i++ {
+		c <- rand.Intn(1000)
+	}
 }
 
-func return_() {
-	return
+func forever(c chan int) {
+	for {
+		c <- rand.Intn(1000)
+	}
+}
+
+func on_event(c chan int, s chan struct{}) {
+	for {
+		select {
+		case _ = <-s:
+			break
+		default:
+			c <- rand.Intn(1000)
+		}
+	}
+}
+
+func on_context(cont context.Context, c chan int) {
+	for {
+		select {
+		case _ = <-cont.Done():
+			break
+		default:
+			c <- rand.Intn(1000)
+		}
+	}
 }
